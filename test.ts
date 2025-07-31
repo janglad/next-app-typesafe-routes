88dbd9c121087bd06fe76e1430fcea5abde9c974
janglad <jan.gladines@gmail.com>
2025-07-31 10:05:48 +0200

    fix removed import

--- test.ts
+++ test.ts
@@ -1,11 +1,5 @@
 import * as z from "zod";
-import {
-  getRoute,
-  layout,
-  page,
-  Router,
-  type AllPaths,
-} from "./src/routing.js";
+import { layout, page, Router, type AllPaths } from "./src/routing.js";
 
 const routes = layout({
   path: "",
