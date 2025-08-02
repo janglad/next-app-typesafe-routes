72a78511c096e29f1bc9ac0426441cc078694540
janglad <jan.gladines@gmail.com>
2025-08-02 15:15:13 +0200

    rework tests a bit

--- src/routing.spec.ts
+++ src/routing.spec.ts
@@ -1,48 +1,36 @@
-import { describe, expect, it } from "vitest";
-import { layout, page, Router } from "./routing.js";
+import { assert, describe, expect, it } from "vitest";
+import {
+  layout,
+  page,
+  Router,
+  RoutingNoMatchingRouteError,
+  type RoutingNoMatchingRouteErrorType,
+} from "./routing.js";
 import z from "zod";
 import { parseAsString } from "nuqs";
 
-const routes = page({
+const routes2 = page({
   path: "",
   children: [
-    page({
-      path: "staticNoChildren",
-    }),
-    page({
-      path: "staticPageWithQuery",
-      query: {
-        page: {
-          pageParam: parseAsString,
-        },
-        layout: {
-          layoutParam: parseAsString,
-        },
-      },
-      children: [
-        page({
-          path: "childPage",
-        }),
-      ],
-    }),
     layout({
       path: "staticLayout",
       children: [
-        page({
-          path: "world",
-        }),
         layout({
-          path: "[layoutParam]",
+          path: "[noValidationDynamicLayout]",
           children: [
             page({
-              path: "world",
+              path: "staticPage",
             }),
             page({
-              path: "[idTwo]",
-              params: z.string().transform((val) => val.toUpperCase()),
+              path: "[noValidationDynamicPage]",
               children: [
                 page({
-                  path: "world",
+                  path: "[toUpperCase]",
+                  params: z.string().transform((val) => val.toUpperCase()),
+                }),
+                page({
+                  path: "[uuid]",
+                  params: z.string().uuid(),
                 }),
               ],
             }),
@@ -50,93 +38,166 @@ const routes = page({
         }),
       ],
     }),
+    page({
+      path: "staticPageAndLayoutQuery",
+      query: {
+        layout: {
+          layoutParam: parseAsString,
+        },
+        page: {
+          pageParam: parseAsString,
+        },
+      },
+      children: [
+        page({
+          path: "[dynamicNoValidation]",
+          query: {
+            page: {
+              dynamicPagePageParam: parseAsString,
+            },
+            layout: {
+              dynamicLayoutParam: parseAsString,
+            },
+          },
+        }),
+      ],
+    }),
   ],
 });
-const router = new Router(routes);
+
+const router2 = new Router(routes2);
 
 describe("Router", () => {
-  describe("route", () => {
-    it("should return the correct data for a static route", (args) => {
-      const route = router.route("/staticNoChildren", {}, {});
-      args.annotate(JSON.stringify({ route, routes }, null, 2));
-      expect(route.data).toEqual("/staticNoChildren");
-    });
-    it("should return noMatch error for a non-existent route", (args) => {
-      const route = router.route("/hello" as any, {}, {});
-      args.annotate(JSON.stringify({ route, routes }, null, 2));
-      expect(route.error).toBeDefined();
-      expect(route.error?._tag).toEqual("RoutingNoMatchingRouteError");
-    });
-    it("should return matchedWrongType error for a route that is a layout", (args) => {
-      const route = router.route("/hello" as any, {}, {});
-      args.annotate(JSON.stringify({ route, routes }, null, 2));
-      expect(route.error).toBeDefined();
-      expect(route.error?._tag).toEqual("RoutingNoMatchingRouteError");
-    });
-    it("should return the correct data for a dynamic route without a schema", (args) => {
-      const route = router.route(
-        "/staticLayout/[layoutParam]/world",
-        {
-          layoutParam: "hi",
-        },
-        {}
-      );
-      args.annotate(JSON.stringify({ route, routes }, null, 2));
-      expect(route.data).toEqual("/staticLayout/hi/world");
-    });
-    it("should return the correct data for a dynamic route with a schema", (args) => {
-      const route = router.route(
-        "/staticLayout/[layoutParam]/[idTwo]",
-        {
-          layoutParam: "hi",
-          idTwo: "bye",
-        },
-        {}
-      );
-      args.annotate(JSON.stringify({ route, routes }, null, 2));
-      expect(route.data).toEqual("/staticLayout/hi/BYE");
-    });
-    it("should return the correct url for a route with query params", (args) => {
-      const route = router.route(
-        "/staticPageWithQuery",
-        {},
-        {
-          pageParam: "hi",
-        }
-      );
-      args.annotate(JSON.stringify({ route, routes }, null, 2));
-      expect(route.data).toHaveExactQueryParams({
-        pageParam: "hi",
-      });
-    });
-    it("Should not accept page params for children of that page", (args) => {
-      const route = router.route(
-        "/staticPageWithQuery/childPage",
-        {},
-        {
-          // @ts-expect-error
-          pageParam: "hi",
-          layoutParam: "hi",
-        }
-      );
-      args.annotate(JSON.stringify({ route, routes }, null, 2));
-      expect(route.data).toHaveExactQueryParams({
-        layoutParam: "hi",
-      });
+  it("Should route the root page correctly", (args) => {
+    const res = router2.route("", {}, {});
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.data).toEqual("");
+    expect(res.error).toBeUndefined();
+    expect(res.data).toHaveExactQueryParams({});
+  });
+  it("Should route a route ending with a static page correctly", (args) => {
+    const res = router2.route(
+      "/staticLayout/[noValidationDynamicLayout]/staticPage",
+      {
+        noValidationDynamicLayout: "param1",
+      },
+      {}
+    );
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.data).toEqual("/staticLayout/param1/staticPage");
+    expect(res.error).toBeUndefined();
+    expect(res.data).toHaveExactQueryParams({});
+  });
+  it("Should route a route ending with a dynamic page correctly", (args) => {
+    const res = router2.route(
+      "/staticLayout/[noValidationDynamicLayout]/[noValidationDynamicPage]",
+      {
+        noValidationDynamicLayout: "param1",
+        noValidationDynamicPage: "param2",
+      },
+      {}
+    );
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.data).toEqual("/staticLayout/param1/param2");
+    expect(res.error).toBeUndefined();
+    expect(res.data).toHaveExactQueryParams({});
+  });
+  it("Should use a schema for dynamic routes that define one", (args) => {
+    const res = router2.route(
+      "/staticLayout/[noValidationDynamicLayout]/[noValidationDynamicPage]/[toUpperCase]",
+      {
+        noValidationDynamicLayout: "param1",
+        noValidationDynamicPage: "param2",
+        toUpperCase: "param3",
+      },
+      {}
+    );
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.data).toEqual("/staticLayout/param1/param2/PARAM3");
+    expect(res.error).toBeUndefined();
+    expect(res.data).toHaveExactQueryParams({});
+  });
+  it("Should URL encode params correctly", (args) => {
+    const res = router2.route(
+      "/staticLayout/[noValidationDynamicLayout]/[noValidationDynamicPage]/[toUpperCase]",
+      {
+        noValidationDynamicLayout: "param1",
+        noValidationDynamicPage: "param2",
+        toUpperCase: "param3/param4",
+      },
+      {}
+    );
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.data).toEqual(
+      `/staticLayout/param1/param2/${encodeURIComponent("PARAM3/PARAM4")}`
+    );
+    expect(res.error).toBeUndefined();
+    expect(res.data).toHaveExactQueryParams({});
+  });
+  it("Should return the correct query page params", (args) => {
+    const res = router2.route(
+      "/staticPageAndLayoutQuery",
+      {},
+      {
+        layoutParam: "param1",
+        pageParam: "param2",
+      }
+    );
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.error).toBeUndefined();
+    expect(res.data).toHaveExactQueryParams({
+      layoutParam: "param1",
+      pageParam: "param2",
     });
   });
-  describe("makeParser", () => {
-    it("should return a function that returns the correct data for a static route", (args) => {
-      const parser = router.makeParser("/staticNoChildren");
-      args.annotate(JSON.stringify({ parser, routes }, null, 2));
-      expect(parser({}, {})).toEqual("/staticNoChildren");
+  it("Should properly merge parent page/layout query params", (args) => {
+    const res = router2.route(
+      "/staticPageAndLayoutQuery/[dynamicNoValidation]",
+      {
+        dynamicNoValidation: "param1",
+      },
+      {
+        layoutParam: "param2",
+        // @ts-expect-error -- this should not be accepted
+        pageParam: "param3",
+        dynamicPagePageParam: "param4",
+        dynamicLayoutParam: "param5",
+      }
+    );
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.error).toBeUndefined();
+    expect(res.data).toHaveExactQueryParams({
+      layoutParam: "param2",
+      dynamicPagePageParam: "param4",
+      dynamicLayoutParam: "param5",
     });
-    it("should return a function that returns the correct data for a dynamic route", (args) => {
-      const parser = router.makeParser("/staticLayout/[layoutParam]/[idTwo]");
-      args.annotate(JSON.stringify({ parser, routes }, null, 2));
-      expect(parser({ layoutParam: "hi", idTwo: "bye" }, {})).toEqual(
-        "/staticLayout/hi/BYE"
-      );
-    });
+  });
+  it("Should not allow routing to layout params", (args) => {
+    const res = router2.route(
+      // @ts-expect-error -- this should not be accepted
+      "/staticLayout/[noValidationDynamicLayout]",
+      {
+        noValidationDynamicLayout: "param1",
+      },
+      {
+        layoutParam: "param2",
+      }
+    );
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.error).toBeDefined();
+    expect(res.error?._tag).toEqual("RoutingNoMatchingRouteError");
+    expect(res.data).toBeUndefined();
+    assert(RoutingNoMatchingRouteError.is(res.error));
+    expect(res.error.type).toBe(
+      "matchedWrongType" satisfies RoutingNoMatchingRouteErrorType
+    );
+  });
+  it("Should not allow routing to non existing routes", (args) => {
+    // @ts-expect-error -- this should not be accepted
+    const res = router2.route("/staticLayout/notARoute", {}, {});
+    args.annotate(JSON.stringify(res, null, 2));
+    expect(res.error).toBeDefined();
+    expect(res.error?._tag).toEqual("RoutingNoMatchingRouteError");
+    expect(res.data).toBeUndefined();
   });
 });
